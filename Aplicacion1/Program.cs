c3f85d4c8bef2d0add6ab774d8cac39c55744a42
Zury Amanda Ríos Osoy <zriososoy164@gmail.com>
2021-07-05 20:48:47 -0600

    Aplicación 1

--- Aplicacion1/Program.cs
+++ Aplicacion1/Program.cs
@@ -10,30 +10,66 @@ namespace Aplicacion1
     {
         static void Main(string[] args)
         {
-            char[] dato = new char[3];
-            char buscar;
-            string Op = "si";
+            string[,] dato= new string [5,15];
+            char OP ='s';
             int op;
-            
-            while(Op=="si")
-            {
-                Console.Clear();
-                Console.WriteLine("agenda");
-                Console.WriteLine("\nno.1 Ingresar Contacto \nno.2 Mostrar Contactos \nno.3 Buscar Contacto: ");
 
-                op = int.Parse(Console.ReadLine());
 
-            }
-            string[,] vec = new string[5, 15];
+            while(OP =='s')
+            { 
+                dato(0,0)= 'Nombre';
+                dato(1,0)= 'apellido';
+                dato(2,0)= 'Dirección';
+                dato(3,0)= 'Telefono';
+                dato(4,0)= 'Correo Electrónico';
 
-            for(int f=0; f<15; f++)
-            {
-                for(int c=0; c<5; c++)
+                Console.WriteLine("Que desea realizar? : \nno.1 Ingresar Contacto \nno.2 Mostrar Contacto \nno.3 Buscar");
+                op = int.Parse(ConsoleReadLine());
+
+
+                if (op==1)
                 {
-                    Console.Write("");
-                    vec[c, f] = Console.ReadLine();
+                  Console.Clear();
+                  Console.WriteLine("Ingrese los siguientes datos: Nombre, Apellido, Dirección, Télefono, Correo Electrónico \n");
+
+                  for (int f=1; f<15; f++)
+                  {
+                    for (int c=0; c<5; c++ )
+                    {
+                      Console.Write("Ingrese Nota (Valor" +(f) + ", Dato: " + (c+1) + "):" );
+                      dato (c,f)= Console.ReadLine();
+                    }
+                  }
                 }
+
+                if (op==2)
+                {
+                  Console.Clear();
+                  Console.WriteLine("\n Datos  de contactos: ");
+
+                  for (int f=0; f<15; f++)
+                  {
+                    Console.WriteLine(dato(0,f) + " " + dato (1,f) + " " + dato(2,f) + " " + dato (3,f) + " " + dato (4,f));
+                  }
+                }
+
+                if (op==3)
+                {
+                  Console.Clear();
+                  Console.WriteLine("Elija el número de contacto que desea encontrar: \n");
+                  
+                    int j;
+                    j=int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("Contacto que buscaba: " + dato(0,a) + " " + dato(1,a) + " " + dato(2,a) +
+                        " " + dato(3,a) + " " + dato(4,a))
+                }
+
+                Console.ReadKey();
+
             }
-        }
-    }
+
+            
+           
+    }   }
 }
